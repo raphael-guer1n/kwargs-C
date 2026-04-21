5574d1df37669a3dd7fbb33831cd44ffcce22388
raphael <raphael.guerin@epitech.eu>
2026-04-21 16:12:27 +0200

    feat(header): add header kw_dup_policy_t

--- kwargs.h
+++ kwargs.h
@@ -2,6 +2,9 @@
 #define KWARGS_H
 
 #include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef enum {
     KW_OK = 0,
@@ -9,7 +12,11 @@ typedef enum {
     KW_ERR_INVALID_INT,
     KW_ERR_INVALID_DOUBLE,
     KW_ERR_INVALID_BOOL,
-    KW_ERR_UNSUPPORTED_TYPE
+    KW_ERR_UNSUPPORTED_TYPE,
+    KW_ERR_PARSE_MISSING_VALUE,
+    KW_ERR_PARSE_EMPTY_KEY,
+    KW_ERR_PARSE_DUPLICATE_KEY,
+    KW_ERR_ALLOC
 } kw_error_t;
 
 typedef enum {
@@ -20,14 +27,48 @@ typedef enum {
     KW_TYPE_INVALID
 } kw_type_t;
 
+typedef enum {
+    KW_DUP_FIRST = 0,
+    KW_DUP_LAST,
+    KW_DUP_ERROR
+} kw_dup_policy_t;
+
 typedef struct {
-    int argc;
-    char **argv;
+    kw_dup_policy_t dup_policy;
+    int strip_dashes;
+} kwargs_options_t;
+
+typedef struct {
+    char *key;
+    const char *value;
+} kw_entry_t;
+
+typedef struct {
+    kw_entry_t *entries;
+    size_t count;
+    size_t capacity;
     kw_error_t error;
     const char *error_key;
+    kw_dup_policy_t dup_policy;
 } kwargs_t;
 
-void kwargs_init(kwargs_t *kwargs, int argc, char **argv);
+void kwargs_options_default(kwargs_options_t *opts);
+
+kw_error_t kwargs_init_from_args(
+    kwargs_t *kwargs,
+    int argc,
+    char **argv,
+    const kwargs_options_t *opts
+);
+
+kw_error_t kwargs_init_from_array(
+    kwargs_t *kwargs,
+    size_t count,
+    char **items,
+    const kwargs_options_t *opts
+);
+
+void kwargs_destroy(kwargs_t *kwargs);
 
 kw_error_t kwargs_get(
     kwargs_t *kwargs,
@@ -38,13 +79,13 @@ kw_error_t kwargs_get(
 
 const char *kwargs_error_string(const kwargs_t *kwargs);
 
-#define KW_TYPEOF_OUT(ptr) _Generic((ptr), \
-    int *: KW_TYPE_INT, \
-    double *: KW_TYPE_DOUBLE, \
-    bool *: KW_TYPE_BOOL, \
-    const char **: KW_TYPE_STR, \
-    char **: KW_TYPE_STR, \
-    default: KW_TYPE_INVALID \
+#define KW_TYPEOF_OUT(ptr) _Generic((ptr),  \
+    int *: KW_TYPE_INT,                     \
+    double *: KW_TYPE_DOUBLE,               \
+    bool *: KW_TYPE_BOOL,                   \
+    const char **: KW_TYPE_STR,             \
+    char **: KW_TYPE_STR,                   \
+    default: KW_TYPE_INVALID                \
 )
 
 #define KW_GET(kwargs, key, out_ptr) \
